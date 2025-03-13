0f1d4b6799286926ace4f9678d49d5d0ba325ad2
Andres Alarcon <aalarcon@transfiriendo.com>
2025-03-13 09:05:57 -0500

    First commit

---
+++ PruebaTecnica.Infrastructure/Context/ApplicationDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnica.Domain.Entities;
+
+namespace PruebaTecnica.Infrastructure.Context
+{
+    public class ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
